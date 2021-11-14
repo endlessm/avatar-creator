fe9047394b72ec7224252bdd3c375262609d0d4c
Daniel García Moreno <dani@danigm.net>
2021-11-14 13:37:14 +0100

    Add filters

--- avatar_creator/main_window.py
+++ avatar_creator/main_window.py
@@ -37,6 +37,7 @@ class MainWindow(Gtk.ApplicationWindow):
     modules = Gtk.Template.Child()
     scrolled_window = Gtk.Template.Child()
     button_box = Gtk.Template.Child()
+    filter_box = Gtk.Template.Child()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -44,7 +45,15 @@ class MainWindow(Gtk.ApplicationWindow):
         self.set_titlebar(self.headerbar)
         self.custom_css()
 
+        self._all_colors = set()
+        self._all_types = set()
+        self._all_placements = set()
+
+        self._modules_groups = {}
+        self.filters = {}
+        self.flowboxes = []
         self.populate_modules(DEFAULT_VARIANT)
+        self.populate_filters()
 
     def custom_css(self):
         css_provider = Gtk.CssProvider()
@@ -62,16 +71,18 @@ class MainWindow(Gtk.ApplicationWindow):
             group_path = os.path.join(assets, group)
             images = os.listdir(group_path)
             images = [ModuleImage(os.path.join(group_path, i)) for i in images]
-            modules_groups[group] = images
+            self._modules_groups[group] = images
 
-        for g, images in modules_groups.items():
+        for g, images in self._modules_groups.items():
             flowbox = Gtk.FlowBox()
+            flowbox.set_filter_func(self._modules_filter)
             label = Gtk.Label()
             label.set_markup(f'<b>{g}</b>')
             label.set_xalign(0.0)
 
             self.modules.append(label)
             self.modules.append(flowbox)
+            self.flowboxes.append(flowbox)
 
             # Buttons
             group_button = Gtk.Button(label=g)
@@ -81,6 +92,49 @@ class MainWindow(Gtk.ApplicationWindow):
             for i, img in enumerate(images):
                 img.connect('clicked', self._on_module_image_clicked)
                 flowbox.insert(img, i)
+                self._all_colors.add(img.color)
+                self._all_types.add(img.type)
+                self._all_placements.add(img.placement)
+
+    def _invalidate_filter(self, widget, data):
+        for f in self.flowboxes:
+            f.invalidate_filter()
+
+    def populate_filters(self):
+        color = Gtk.DropDown.new_from_strings(['Color'] + list(self._all_colors))
+        color.connect('notify::selected-item', self._invalidate_filter)
+        self.filters['color'] = color
+
+        typef = Gtk.DropDown.new_from_strings(['Type'] + list(self._all_types))
+        typef.connect('notify::selected-item', self._invalidate_filter)
+        self.filters['type'] = typef
+
+        placement = Gtk.DropDown.new_from_strings(['Placement'] + list(self._all_placements))
+        placement.connect('notify::selected-item', self._invalidate_filter)
+        self.filters['placement'] = placement
+
+        self.filter_box.append(color)
+        self.filter_box.append(typef)
+        self.filter_box.append(placement)
+
+    def _filter_by(self, child, filter_name, default):
+        filter = self.filters[filter_name]
+        value = filter.get_selected_item().get_string()
+        if value != default:
+            return getattr(child, filter_name) == value
+        return True
+
+    def _modules_filter(self, child):
+        if not 'color' in self.filters:
+            return True
+
+        show = True
+        object = child.get_child()
+        show = show and self._filter_by(object, 'color', 'Color')
+        show = show and self._filter_by(object, 'type', 'Type')
+        show = show and self._filter_by(object, 'placement', 'Placement')
+
+        return show
 
     def scroll_to_group(self, button, group):
         adj = self.scrolled_window.get_vadjustment()
